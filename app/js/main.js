a90672daa6ff2cf17b56ee95154d404880ed98ff
LuckyMan01 <55346306+LuckyMan01@users.noreply.github.com>
2020-04-08 14:03:28 +0300

    end header

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,12 @@ $(function(){
 
     var mixer = mixitup('.container');
 
+    $('.header__slider-inner').slick({ 
+        slidesToShow: 1,
+        slidesToScroll: 1, 
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+      });
 
 });
