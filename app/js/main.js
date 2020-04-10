fb0caad85a7e32a84b299250819fcb7d287c9dde
LuckyMan01 <55346306+LuckyMan01@users.noreply.github.com>
2020-04-10 16:06:44 +0300

    facts

--- app/js/main.js
+++ app/js/main.js
@@ -20,4 +20,20 @@ $(function(){
         nextArrow: $('.product-next')
       });
 
+      $('.follower__inner').slick({ 
+        slidesToShow: 3,
+        slidesToScroll: 3, 
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.follower-prev'),
+        nextArrow: $('.follower-next')
+      });
+      
+      $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+      });
+
 });
